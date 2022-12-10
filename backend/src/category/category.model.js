65d00890cceece6c9f2c4822a6b8520d47d66a93
Enick Jhony <enick.jhony@gmail.com>
2022-12-09 20:18:10 -0400

    Criando o Model de Category.

---
+++ backend/src/category/category.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+// Criando o model de Category apenas com o Nome.
+const categorySchema = new mongoose.Schema({
+    name: {type: String, require: true},
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
